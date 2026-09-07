66f9846f074da97bea2681a6568b549e2bdda93a
RoxyZZZ <1927139831@qq.com>
2026-09-08 07:21:53 +0800

    feat(compact): 优化压缩工具结果、多模态消息时的默认提示词

--- src/utils/compact/strategy.ts
+++ src/utils/compact/strategy.ts
@@ -10,7 +10,7 @@ const defaultReplacerOfToolResultContent: Compact.ReplacerOfToolResultContent =
 		const { model } = options ?? {};
 		const messages: Message[] = [
 			{ role: "user", content },
-			{ role: "user", content: "请用一两句话简述上条消息" },
+			{ role: "user", content: "请用一句话简述上面这条消息（直击要点，不要添加任何前言后语）" },
 		];
 
 		let simplifiedContent = "";
@@ -71,7 +71,7 @@ const defaultReplacerOfMediaContent: Compact.ReplacerOfMediaContent = async (
 	const { model } = options ?? {};
 	const messages: Message[] = [
 		{ role: "user", content },
-		{ role: "user", content: "请用一两句话简述上方的多模态消息" },
+		{ role: "user", content: "请用一句话简述上面这条消息（直击要点，不要添加任何前言后语）" },
 	];
 
 	let simplifiedContent = "";
